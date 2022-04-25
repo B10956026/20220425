dd08218734db749e2c7dd5075b2e6e19c22960f3
B10956026 <84659977+B10956026@users.noreply.github.com>
2022-04-26 00:04:49 +0800

    TCP_Client

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -16,7 +16,7 @@ namespace _20220425
         {
             if (disposing && (components != null))
             {
-                components.Dispose();
+                components.Dispose(); 
             }
             base.Dispose(disposing);
         }
@@ -29,63 +29,52 @@ namespace _20220425
         /// </summary>
         private void InitializeComponent()
         {
-            this.lbl_onlineuser = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lbl_player = new System.Windows.Forms.Label();
             this.lbl_IP = new System.Windows.Forms.Label();
             this.lbl_Port = new System.Windows.Forms.Label();
             this.tb_IP = new System.Windows.Forms.TextBox();
             this.tb_Port = new System.Windows.Forms.TextBox();
-            this.btn_Run = new System.Windows.Forms.Button();
+            this.btn_Login_Server = new System.Windows.Forms.Button();
+            this.tb_player = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // lbl_onlineuser
+            // lbl_player
             // 
-            this.lbl_onlineuser.AutoSize = true;
-            this.lbl_onlineuser.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.lbl_onlineuser.Location = new System.Drawing.Point(13, 19);
-            this.lbl_onlineuser.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lbl_onlineuser.Name = "lbl_onlineuser";
-            this.lbl_onlineuser.Size = new System.Drawing.Size(134, 31);
-            this.lbl_onlineuser.TabIndex = 0;
-            this.lbl_onlineuser.Text = "線上使用者";
-            // 
-            // listBox1
-            // 
-            this.listBox1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 30;
-            this.listBox1.Location = new System.Drawing.Point(18, 63);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(256, 244);
-            this.listBox1.TabIndex = 1;
+            this.lbl_player.AutoSize = true;
+            this.lbl_player.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbl_player.Location = new System.Drawing.Point(16, 126);
+            this.lbl_player.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_player.Name = "lbl_player";
+            this.lbl_player.Size = new System.Drawing.Size(110, 31);
+            this.lbl_player.TabIndex = 0;
+            this.lbl_player.Text = "玩家名稱";
             // 
             // lbl_IP
             // 
             this.lbl_IP.AutoSize = true;
             this.lbl_IP.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.lbl_IP.Location = new System.Drawing.Point(310, 63);
+            this.lbl_IP.Location = new System.Drawing.Point(18, 30);
             this.lbl_IP.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_IP.Name = "lbl_IP";
-            this.lbl_IP.Size = new System.Drawing.Size(115, 31);
+            this.lbl_IP.Size = new System.Drawing.Size(108, 31);
             this.lbl_IP.TabIndex = 2;
-            this.lbl_IP.Text = "Server IP";
+            this.lbl_IP.Text = "伺服器IP";
             // 
             // lbl_Port
             // 
             this.lbl_Port.AutoSize = true;
             this.lbl_Port.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.lbl_Port.Location = new System.Drawing.Point(285, 130);
+            this.lbl_Port.Location = new System.Drawing.Point(465, 22);
             this.lbl_Port.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_Port.Name = "lbl_Port";
-            this.lbl_Port.Size = new System.Drawing.Size(141, 31);
+            this.lbl_Port.Size = new System.Drawing.Size(134, 31);
             this.lbl_Port.TabIndex = 3;
-            this.lbl_Port.Text = "Server Port";
+            this.lbl_Port.Text = "伺服器Port";
             // 
             // tb_IP
             // 
             this.tb_IP.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.tb_IP.Location = new System.Drawing.Point(435, 51);
+            this.tb_IP.Location = new System.Drawing.Point(142, 27);
             this.tb_IP.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tb_IP.Name = "tb_IP";
             this.tb_IP.Size = new System.Drawing.Size(238, 39);
@@ -94,41 +83,50 @@ namespace _20220425
             // tb_Port
             // 
             this.tb_Port.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.tb_Port.Location = new System.Drawing.Point(435, 126);
+            this.tb_Port.Location = new System.Drawing.Point(615, 18);
             this.tb_Port.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tb_Port.Name = "tb_Port";
             this.tb_Port.Size = new System.Drawing.Size(238, 39);
             this.tb_Port.TabIndex = 5;
             // 
-            // btn_Run
+            // btn_Login_Server
             // 
-            this.btn_Run.AutoSize = true;
-            this.btn_Run.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.btn_Run.Location = new System.Drawing.Point(435, 262);
-            this.btn_Run.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.btn_Run.Name = "btn_Run";
-            this.btn_Run.Size = new System.Drawing.Size(240, 46);
-            this.btn_Run.TabIndex = 6;
-            this.btn_Run.Text = "啟動伺服器";
-            this.btn_Run.UseVisualStyleBackColor = true;
-            this.btn_Run.Click += new System.EventHandler(this.btn_Run_Click);
+            this.btn_Login_Server.AutoSize = true;
+            this.btn_Login_Server.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn_Login_Server.Location = new System.Drawing.Point(615, 118);
+            this.btn_Login_Server.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_Login_Server.Name = "btn_Login_Server";
+            this.btn_Login_Server.Size = new System.Drawing.Size(240, 46);
+            this.btn_Login_Server.TabIndex = 6;
+            this.btn_Login_Server.Text = "登入伺服器";
+            this.btn_Login_Server.UseVisualStyleBackColor = true;
+            this.btn_Login_Server.Click += new System.EventHandler(this.btn_Login_Server_Click);
+            // 
+            // tb_player
+            // 
+            this.tb_player.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tb_player.Location = new System.Drawing.Point(142, 122);
+            this.tb_player.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tb_player.Name = "tb_player";
+            this.tb_player.Size = new System.Drawing.Size(238, 39);
+            this.tb_player.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(688, 321);
-            this.Controls.Add(this.btn_Run);
+            this.ClientSize = new System.Drawing.Size(878, 186);
+            this.Controls.Add(this.tb_player);
+            this.Controls.Add(this.btn_Login_Server);
             this.Controls.Add(this.tb_Port);
             this.Controls.Add(this.tb_IP);
             this.Controls.Add(this.lbl_Port);
             this.Controls.Add(this.lbl_IP);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.lbl_onlineuser);
+            this.Controls.Add(this.lbl_player);
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "TCP Server";
+            this.Text = "TCP 客戶端";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -137,13 +135,13 @@ namespace _20220425
 
         #endregion
 
-        private System.Windows.Forms.Label lbl_onlineuser;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label lbl_player;
         private System.Windows.Forms.Label lbl_IP;
         private System.Windows.Forms.Label lbl_Port;
         private System.Windows.Forms.TextBox tb_IP;
         private System.Windows.Forms.TextBox tb_Port;
-        private System.Windows.Forms.Button btn_Run;
+        private System.Windows.Forms.Button btn_Login_Server;
+        private System.Windows.Forms.TextBox tb_player;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,21 +10,16 @@ using System.Windows.Forms;
 
 using System.Net; // 匯入網路通訊協定相關函數
 using System.Net.Sockets; // 匯入網路插座功能函數
-using System.Threading; // 匯入多執行緒功能函數
-using System.Collections; // 匯入集合物件
 
 namespace _20220425
 {
     public partial class Form1 : Form
     {
-        TcpListener Server; // 伺服端網路監聽器(相當於電話總機)
-        Socket Client; // 給客戶用的連線物件(相當於電話分機)
-        Thread Th_Svr; // 伺服器監聽用執行緒(電話總機開放中)
-        Thread Th_Clt; // 客戶用的通話執行緒(電話分機連線中)
-        Hashtable HT = new Hashtable(); // 儲存客戶名稱與通訊物件的集合物件(雜湊表)(key:Name, Socket)
+        Socket T; // 通訊物件
+        string User; // 使用者
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent(); 
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -32,68 +27,41 @@ namespace _20220425
 
         }
 
-        private void btn_Run_Click(object sender, EventArgs e)
+        // 登入伺服器
+        private void btn_Login_Server_Click(object sender, EventArgs e)
         {
-            CheckForIllegalCrossThreadCalls = false; // 忽略跨執行緒處理的錯誤(允許跨執行緒存取變數)
-            Th_Svr = new Thread(ServerSub); // 宣告監聽執行緒(副程式ServerSub)
-            Th_Svr.IsBackground = true; // 設定為背景執行緒
-            Th_Svr.Start(); // 啟動監聽執行緒
-            btn_Run.Enabled = false; // 讓按鍵無法使用(不能重複啟動伺服器) 
-        }
-
-        // 接受客戶連線要求的程式(如同電話總機)，針對每一客戶會建立一個連線，以及獨立執行緒
-        private void ServerSub()
-        {
-            IPEndPoint EP = new IPEndPoint(IPAddress.Parse(tb_IP.Text), int.Parse(tb_Port.Text)); // Server IP 和 Port
-            Server = new TcpListener(EP); // 建立伺服端監聽器(總機)
-            Server.Start(100); // 啟動監聽設定允許最多連線數100人
-            while (true) // 無限迴圈監聽連線要求
+            string IP = tb_IP.Text; // 伺服器IP
+            int Port = int.Parse(tb_Port.Text); // 伺服器Port
+            IPEndPoint EP = new IPEndPoint(IPAddress.Parse(IP), Port); // 伺服器的連線端點資訊
+            T = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); // 建立可以雙向通訊的TCP連線
+            User = tb_player.Text; // 使用者名稱 
+            try
             {
-                Client = Server.AcceptSocket(); // 建立此客戶的連線物件Client
-                Th_Clt = new Thread(Listen); // 建立監聽這個客戶連線的獨立執行緒
-                Th_Clt.IsBackground = true; // 設定為背景執行緒
-                Th_Clt.Start(); // 開始執行緒的運作
+                T.Connect(EP); // 連上伺服器的端點EP(類似撥號給電話總機)
+                Send("0" + User); // 連線後隨即傳送自己的名稱給伺服器
             }
-        }
-
-        // 監聽客戶訊息的程式
-        private void Listen()
-        {
-            Socket Sck = Client; // 複製Client通訊物件到個別客戶專用物件Sck
-            Thread Th = Th_Clt; // 複製執行緒Th_Clt到區域變數Th
-            while (true) // 持續監聽客戶傳來的訊息
+            catch (Exception)
             {
-                try // 用 Sck 來接收此客戶訊息，inLen 是接收訊息的 byte 數目
-                {
-                    byte[] B = new byte[1023]; // 建立接收資料用的陣列，長度須大於可能的訊息
-                    int inLen = Sck.Receive(B); // 接收網路資訊(byte陣列)
-                    string Msg = Encoding.Default.GetString(B, 0, inLen); // 翻譯實際訊息(長度inLen)
-                    string Cmd = Msg.Substring(0, 1); // 取出命令碼 (第一個字)
-                    string Str = Msg.Substring(1); // 取出命令碼之後的訊息
-                    switch (Cmd) // 依據命令碼執行功能
-                    {
-                        case "0": // 有新使用者上線：新增使用者到名單中
-                            HT.Add(Str, Sck); // 連線加入雜湊表，Key:使用者，Value:連線物件(Socket)
-                            listBox1.Items.Add(Str); // 加入上線者名單
-                            break;
-                        case "9":
-                            HT.Remove(Str); // 移除使用者名稱為Name的連線物件
-                            listBox1.Items.Remove(Str); // 自上線者名單移除Name
-                            Th.Abort(); // 結束此客戶的監聽執行緒
-                            break;
-                    }
-                }
-                catch (Exception)
-                {
-                    // 有錯誤時忽略，通常是客戶端無預警強制關閉程式，測試階段常發生
-                }
+                MessageBox.Show("無法連上伺服器！"); //連線失敗時顯示訊息
+                return;
             }
+            btn_Login_Server.Enabled = false; // 讓連線按鍵失效，避免重複連線 
         }
 
-        // 關閉視窗時
+        // 傳送訊息給 Server (Send Message to the Server)
+        private void Send(string Str)
+        {
+            byte[] B = Encoding.Default.GetBytes(Str); // 翻譯字串Str為Byte陣列B
+            T.Send(B, 0, B.Length, SocketFlags.None); // 使用連線物件傳送資料
+        }
+        // 關閉視窗代表離線登出
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            Application.ExitThread(); // 關閉所有執行緒
+            if (btn_Login_Server.Enabled == false)
+            {
+                Send("9" + User); // 傳送自己的離線訊息給伺服器
+                T.Close(); // 關閉網路通訊器T
+            }
         }
     }
 }
